97f3804e288adb85573e25f915dea8f883f2a0b3
alvin almodal <almodalalvin@gmail.com>
2020-08-05 22:16:45 +0800

    added interceptor and global error handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from './services/auth.service';
+import { ErrorInterceptorProvider } from './services/error.interceptor';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [AuthService],
+  providers: [ ErrorInterceptorProvider ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next =>{
       console.log('login successfully');
     }, error => {
-      console.log('failed to login : ' + error);
+      console.log(error);
     });
   }
 
